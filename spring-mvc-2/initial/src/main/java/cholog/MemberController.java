9ca177b4803a4734484211e3555ed2e0e5cbb128
corjqnrl <coqnrl@ewha.ac.kr>
2025-10-02 23:49:59 +0900

    update 학습 테스트

--- spring-mvc-2/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-2/initial/src/main/java/cholog/MemberController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class MemberController {
@@ -34,14 +35,14 @@ public class MemberController {
     }
 
     @PutMapping("/members/{id}")
-    public ResponseEntity<Void> update() {
+    public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody Member newMember) {
         // TODO: member의 수정 정보와 url 상의 id 정보를 받아 member 정보를 수정한다.
         Member member = members.stream()
-            .filter(it -> Objects.equals(it.getId(), null))
+            .filter(it -> Objects.equals(it.getId(), id))
             .findFirst()
             .orElseThrow(RuntimeException::new);
 
-        member.update(null);
+        member.update(newMember);
         return null;
     }
 
