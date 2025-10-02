943cd05606ac1d8602476e51469f25c06ab11a6b
corjqnrl <coqnrl@ewha.ac.kr>
2025-10-02 23:56:26 +0900

    delete 학습 테스트

--- spring-mvc-2/initial/src/main/java/cholog/MemberController.java
+++ spring-mvc-2/initial/src/main/java/cholog/MemberController.java
@@ -47,15 +47,15 @@ public class MemberController {
     }
 
     @DeleteMapping("/members/{id}")
-    public ResponseEntity<Void> delete() {
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
         // TODO: url 상의 id 정보를 받아 member를 삭제한다.
         Member member = members.stream()
-            .filter(it -> Objects.equals(it.getId(), null))
+            .filter(it -> Objects.equals(it.getId(), id))
             .findFirst()
             .orElseThrow(RuntimeException::new);
 
         members.remove(member);
 
-        return null;
+        return ResponseEntity.noContent().build();
     }
 }
